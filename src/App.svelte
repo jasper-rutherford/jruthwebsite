8de93fd723c6a39d703e795f5356a1de454094ca
Jasper Rutherford <jasperrutherford874@gmail.com>
2025-04-05 20:46:33 -0500

    again

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,5 @@
 <script>
   import twoTimes from './assets/Snapchat-7137920.jpg'
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
