2fb6bf9ec04f440a75bd48ff5e4bf4031a9ef3ef
Jasper Rutherford <jasperrutherford874@gmail.com>
2025-04-05 22:30:26 -0500

    config port to 80

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    port: 80,
+    host: true, // allows access from other devices
+  },
 })
