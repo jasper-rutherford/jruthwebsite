459e7a2978f0c12fa318900c1d3c46cbb625618d
Jasper Rutherford <jasperrutherford874@gmail.com>
2025-04-05 20:27:39 -0500

    in de beninging

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import twoTimes from './assets/Snapchat-7137920.jpg'
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>hey jasper how many times have you set up this website</h1>
+  
+  <div>
+    <img src={twoTimes} class="two" alt="two times dammit" />
+  </div>
+</main>
+
+<style>
+  .two {
+    height: 50vh;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<!-- TODO(jruth): kill this file after i read it --> 
+
+<script>
+  let count = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button onclick={increment}>
+  count is {count}
+</button>
